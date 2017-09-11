f210ff4be1782b65d21a62baa27a2956aba1dbe1
Di CHEN <stanley.chendi@gmail.com>
2017-09-10 22:47:53 -0700

    Submission_to_Pex

--- stanley_coding_challenge_solution.js
+++ stanley_coding_challenge_solution.js
@@ -36,57 +36,50 @@ const errors = Immutable.fromJS({
 
 
 
-let arr_error_content = [];
-let key_indicator = -1;
 
-// Extract all error message with level 1 key name, and store them in an array
-function loopMap(inputMap, originInputMap){
-  inputMap.entrySeq().forEach((e) => {
-    if (originInputMap.get(e[0]) !== undefined) {
-      key_indicator++; 
-    }
-    if(!Immutable.Map.isMap(e[1])) {
-      e[1].forEach((arrElement) => {
-        if(Immutable.Map.isMap(arrElement)) {
-          return loopMap(arrElement, originInputMap);
-        } else {
-          arr_error_content.push(key_indicator + " " + ": " + arrElement + ".");
-          return;
-        }
-      });
-    } else {       
-      return loopMap(e[1], originInputMap);
-    }
-  });
-}
-
-
-// Delete duplicates from the array, which is a error message container
+// Delete duplicates from the array
 function uniq(a) {
    return Array.from(new Set(a));
 }
 
 
-// 
-function resetParameters() {
-  arr_error_content = [];
-  key_indicator = -1;
-}
-
-
+// Return an array with all error messages with level 1 key name
 function getErrorContent(inputMap) {
-   let arr_error_content = [];
-   let key_indicator = -1;
+  let arr_error_content = [];
+  let key_indicator = -1;
 
-
-
+  // Extract all error message with level 1 key name, and store them in an array
+  function loopMap(inputMap, originInputMap){
+    inputMap.entrySeq().forEach((e) => {
+      if (originInputMap.get(e[0]) !== undefined) {
+        key_indicator++; 
+      }
+      if(!Immutable.Map.isMap(e[1])) {
+        e[1].forEach((arrElement) => {
+          if(Immutable.Map.isMap(arrElement)) {
+            return loopMap(arrElement, originInputMap);
+          } else {
+            arr_error_content.push(key_indicator + " " + ": " + arrElement + ".");
+            return;
+          }
+        });
+      } else {       
+        return loopMap(e[1], originInputMap);
+      }
+    });
+  }
 
   loopMap(inputMap, inputMap);
+  
+  // Delete duplicates from the array, which is a error message container
   arr_error_content = uniq(arr_error_content);
+
   return arr_error_content;
 }
 
 
+// In a map, set all values to blank except for those whose key names were specified
+// in the second argument.
 function resetMap(inputMap, keyArr){
   inputMap.keySeq().forEach((key) => {
     if(keyArr.indexOf(key) == -1) {
@@ -100,6 +93,7 @@ function resetMap(inputMap, keyArr){
 function transformErrors(inputMap, keyArr) {
   let resettedErrors = resetMap(inputMap, keyArr);
   let errorContents = getErrorContent(inputMap)
+
   errorContents.forEach((element) => {
     let currKeyIndex = Number(element.substring(0, element.indexOf(" ")));
     let currKey = Array.from(errors.keys())[currKeyIndex];
@@ -109,6 +103,7 @@ function transformErrors(inputMap, keyArr) {
     } 
   });
   
+  // Modify error messages for those whose structure should be kept
   stringified_resettedErrors = JSON.stringify(resettedErrors);
   stringified_resettedErrors = stringified_resettedErrors.replace(/\["/g, '"');
   stringified_resettedErrors = stringified_resettedErrors.replace(/"]/g, '."');
@@ -117,11 +112,9 @@ function transformErrors(inputMap, keyArr) {
   return errors_final;
 }
 
-
+// Two testing case
 console.log(transformErrors(errors, ['urls', 'url']));
 console.log(transformErrors(errors, []));
-
-//test for github
 
 
 
